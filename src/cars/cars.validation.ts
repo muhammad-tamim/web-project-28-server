d7685a62f70bcb1def132d766b5f5fb851eb5d1c
muhammad-tamim <contact2tamim@gmail.com>
2026-02-01 21:21:22 +0600

    refactor(cars.validations): impove create and update schema

--- src/cars/cars.validation.ts
+++ src/cars/cars.validation.ts
@@ -2,6 +2,9 @@ import { z } from 'zod'
 
 export const createCarSchema = z.object({
     name: z.string().min(2),
+    brand: z.string().min(2),
+    model: z.string().min(2),
+    year: z.number().int().min(2000).max(new Date().getFullYear()),
     description: z.string().min(10),
     dailyRentalPrice: z.coerce.number().positive(),
     availability: z.boolean(),
@@ -10,12 +13,6 @@ export const createCarSchema = z.object({
     imageUrl: z.url(),
     location: z.string().min(2),
     ownerEmail: z.email(),
-    bookingCount: z.number().int().nonnegative().optional(),
-    bookingStatus: z.boolean().optional(),
-    createdAt: z.date().optional(),
 })
 
-export const updateCarSchema = createCarSchema.omit({
-    bookingCount: true,
-    createdAt: true,
-}).partial();
+export const updateCarSchema = createCarSchema.omit({ ownerEmail: true }).partial()
