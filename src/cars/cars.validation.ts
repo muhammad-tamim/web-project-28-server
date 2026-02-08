df2ff00087b0788faa4be8124bb6dde6e1e9f3d0
muhammad-tamim <contact2tamim@gmail.com>
2026-02-08 20:25:35 +0600

    refactor(cars validation): remove location field

--- src/cars/cars.validation.ts
+++ src/cars/cars.validation.ts
@@ -6,15 +6,14 @@ export const createCarSchema = z.object({
     name: z.string().min(2),
     model: z.string().min(2),
     year: z.number().int().min(2000).max(new Date().getFullYear()),
-    brand: z.string().refine(val => ObjectId.isValid(val)),
-    type: z.string().refine(val => ObjectId.isValid(val)),
+    brand: z.string(),
+    category: z.string(),
     description: z.string().min(10),
     dailyRentalPrice: z.coerce.number().positive(),
-    availability: z.boolean(),
+    availability: z.boolean().optional().default(true),
     registrationNumber: z.string().min(3),
     features: z.array(z.string().min(1)).min(1),
     imageUrl: z.url(),
-    location: z.string().min(2),
     ownerEmail: z.email(),
 })
 
