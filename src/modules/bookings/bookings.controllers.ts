8aa62dc46b4071c0237b3ea5ce1918601989973c
muhammad-tamim <contact2tamim@gmail.com>
2026-02-14 13:57:40 +0600

    feat(modules/bookings/controller): remove create booking controller

--- src/modules/bookings/bookings.controllers.ts
+++ src/modules/bookings/bookings.controllers.ts
@@ -1,19 +1,6 @@
+// bookings.controllers.ts
 import { Request, Response } from "express"
 import { bookingsService } from "./bookings.services.js"
-
-export const createBooking = async (req: Request, res: Response) => {
-    try {
-        const result = await bookingsService.create(req.body)
-        res.status(201).send({
-            success: true,
-            message: "Booking created successfully",
-            result
-        })
-    }
-    catch (err: any) {
-        res.status(500).send({ message: err.message })
-    }
-}
 
 export const getBookings = async (req: Request, res: Response) => {
     try {
