156a4c1c656c06a89851d8b29762fe4987d4cb29
muhammad-tamim <contact2tamim@gmail.com>
2026-02-14 13:53:51 +0600

    feat(app): add stripe

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+// app.ts
 import express from 'express'
 import cors from 'cors'
 import { carsRoutes } from './modules/cars/cars.routes.js'
 import { bookingsRoutes } from './modules/bookings/bookings.routes.js'
 import { brandsRoutes } from './modules/carBrands/brands.routes.js'
 import { categoriesRoutes } from './modules/carCategories/categories.routes.js'
+import { stripeRoutes } from './modules/stripe/stripe.routes.js'
 
 const app = express()
 
@@ -14,6 +16,7 @@ app.use('/cars', carsRoutes)
 app.use('/brands', brandsRoutes)
 app.use('/categories', categoriesRoutes)
 app.use('/bookings', bookingsRoutes)
+app.use('/stripe', stripeRoutes);
 
 app.get('/', (_req, res) => {
     res.send('Hello World!')
