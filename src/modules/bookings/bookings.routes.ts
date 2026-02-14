e95a6ed46da32061e17e9348cd9a8c8d0b666021
muhammad-tamim <contact2tamim@gmail.com>
2026-02-14 13:57:01 +0600

    refactor(modules/bookings/routes): remove create booking route

--- src/modules/bookings/bookings.routes.ts
+++ src/modules/bookings/bookings.routes.ts
@@ -1,11 +1,11 @@
+// bookings.routes.ts
 import { Router } from "express";
 import { createBookingSchema, updateBookingSchema } from "./bookings.validations.js";
-import { createBooking, deleteBooking, getBookings, updateBooking } from "./bookings.controllers.js";
+import { deleteBooking, getBookings, updateBooking } from "./bookings.controllers.js";
 import { validate } from "../../middlewares/validate.js";
 
 const router = Router()
 
-router.post('/', validate(createBookingSchema), createBooking)
 router.get('/:email', getBookings)
 router.patch('/:id', validate(updateBookingSchema), updateBooking)
 router.delete('/:id', deleteBooking)
