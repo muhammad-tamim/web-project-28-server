a736fc5db873d3dc101eb5fe73b2d8020265afc1
muhammad-tamim <contact2tamim@gmail.com>
2026-02-14 13:59:46 +0600

    feat(modules/bookings/types): add payment info and booking document type

--- src/modules/bookings/bookings.types.ts
+++ src/modules/bookings/bookings.types.ts
@@ -1,5 +1,21 @@
+// bookings.types.ts
 import z from "zod"
 import { createBookingSchema, updateBookingSchema } from "./bookings.validations.js"
 
 export type CreateBookingInput = z.infer<typeof createBookingSchema>
 export type UpdateBookingInput = z.infer<typeof updateBookingSchema>
+
+export type PaymentInfo = {
+    sessionId: string;
+    paymentIntentId: string;
+    amount: number;
+    currency: string;
+    status: string;
+    paidAt: Date;
+};
+
+export type BookingDocument = CreateBookingInput & {
+    totalCost: number;
+    car: any;
+    payment: PaymentInfo;
+};
