bae0a18614777996f6d3cce995ffcbfeaa912655
muhammad-tamim <contact2tamim@gmail.com>
2026-02-14 13:54:24 +0600

    feat(config): add stripe secret key and client  url

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,9 @@ import 'dotenv/config'
 export const env = {
     PORT: process.env.PORT || 3000,
     MONGODB_URI: process.env.MONGODB_URI as string,
-    FB_SERVICE_KEY: process.env.FB_SERVICE_KEY as string
+    FB_SERVICE_KEY: process.env.FB_SERVICE_KEY as string,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    CLIENT_URL: process.env.CLIENT_URL
 }
 
 if (!env.MONGODB_URI) {
