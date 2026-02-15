2f8b8060648ef556915892edf3e9eaa291cf9c89
muhammad-tamim <contact2tamim@gmail.com>
2026-02-15 19:54:46 +0600

    refactor(modules/carCategories/categories.services.ts): rename mongodb collection name from types to categories

--- src/modules/carCategories/categories.services.ts
+++ src/modules/carCategories/categories.services.ts
@@ -2,7 +2,7 @@ import { ObjectId } from "mongodb";
 import { client } from "../../config/db.js";
 import { CreateCategoryInput, UpdateCategoryInput } from "./categories.types.js";
 
-export const categoriesCollection = client.db('web-project-28-DB').collection<CreateCategoryInput>('types')
+export const categoriesCollection = client.db('web-project-28-DB').collection<CreateCategoryInput>('categories')
 
 export const categoriesService = {
 
